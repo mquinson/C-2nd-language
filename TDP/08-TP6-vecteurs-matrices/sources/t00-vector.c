e38c7cfef57974e96c3480233329239a08f8131e
Martin Quinson <martin.quinson@loria.fr>
2018-03-02 23:37:14 +0100

    Renome les premiers fichiers de test

--- TDP/08-TP6-vecteurs-matrices/sources/t00-vector.c
+++ TDP/08-TP6-vecteurs-matrices/sources/t00-vector.c
@@ -4,14 +4,12 @@
 #include "utils_vecteur.h"
 
 int main(void) {
-  vecteur_t *v1;
-  vecteur_t *v2;
 
-  v1 = lit_vecteur("vecteur1.txt");
+  vecteur_t* v1 = lit_vecteur("vecteur1.txt");
   affiche_vecteur(v1);
   liberer_vecteur(v1);
 
-  v2 = lit_vecteur("vecteur2.txt");
+  vecteur_t* v2 = lit_vecteur("vecteur2.txt");
   affiche_vecteur(v2);
   liberer_vecteur(v2);
 
