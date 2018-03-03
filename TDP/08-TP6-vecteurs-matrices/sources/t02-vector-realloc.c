d1aed671021aeab58b04f0b4015ae90a0fad2b0d
Martin Quinson <martin.quinson@loria.fr>
2018-03-03 02:07:33 +0100

    un meilleur test vector-realloc

--- TDP/08-TP6-vecteurs-matrices/sources/t02-vector-realloc.c
+++ TDP/08-TP6-vecteurs-matrices/sources/t02-vector-realloc.c
@@ -5,28 +5,48 @@
 #include "utils_vecteur.h"
 
 int main(void) {
-  vecteur_t *v1, *v2;
-  unsigned i, limite;
 
-  v1 = lit_vecteur("vecteur1.txt");
-  limite = taille_vecteur(v1);
-  for (i = 0; i < limite + 100; i++)
+  printf("Crée un vecteur dynamique de longueur 20 où chaque case est un seul chiffre:\n");
+  vecteur_t* v1 = allouer_vecteur(20);
+  vecteur_rend_dynamique(v1);
+  for (unsigned int i = 0; i < 20; i++)
+    *acces_vecteur(v1, i) = i % 10;
+
+  for (unsigned int i = 0; i < taille_vecteur(v1); i++)
+      if (acces_vecteur(v1, i) != NULL)
+          printf("%g", *acces_vecteur(v1, i));
+  printf("\n\n");
+
+  printf("Ajoute 10 cases derrière, et réaffiche le tout:\n");
+  for (unsigned int i = 20; i < 30; i++)
       if (acces_vecteur(v1, i) != NULL)
           *acces_vecteur(v1, i) = i % 10;
-  for (i = 0; i < limite + 100; i++)
+
+  for (unsigned int i = 0; i < 30; i++)
       if (acces_vecteur(v1, i) != NULL)
           printf("%g", *acces_vecteur(v1, i));
   printf("\n");
   liberer_vecteur(v1);
 
-  v2 = lit_vecteur("vecteur2.txt");
-  limite = taille_vecteur(v2);
-  for (i = limite + 101; i > 0; i--)
-      if (acces_vecteur(v2, i - 1) != NULL)
-          *acces_vecteur(v2, i - 1) = (i - 1) % 10;
-  for (i = limite + 101; i > 0; i--)
-      if (acces_vecteur(v2, i - 1) != NULL)
-          printf("%g", *acces_vecteur(v2, i - 1));
+  printf("\n\n\nCrée un nouveau vecteur dynamique de longueur 20 où chaque case est un seul chiffre:\n");
+  vecteur_t* v2 = allouer_vecteur(20);
+  vecteur_rend_dynamique(v2);
+  for (unsigned int i = 0; i < 20; i++)
+    *acces_vecteur(v2, i) = i % 10;
+
+  for (unsigned int i = 0; i < taille_vecteur(v2); i++)
+      if (acces_vecteur(v2, i) != NULL)
+          printf("%g", *acces_vecteur(v2, i));
+  printf("\n\n");
+
+  printf("Ajoute 10 cases derrière (en commençant par la dernière), et réaffiche le tout:\n");
+  for (unsigned int i = 0; i < 10; i++)
+      if (acces_vecteur(v2, 30-i) != NULL)
+          *acces_vecteur(v2, 30-i) = i % 10;
+
+  for (unsigned int i = 0; i < 30; i++)
+      if (acces_vecteur(v2, i) != NULL)
+          printf("%g", *acces_vecteur(v2, i));
   printf("\n");
   liberer_vecteur(v2);
 
